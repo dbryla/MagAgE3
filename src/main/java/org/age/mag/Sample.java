a05a752d369bde1580bc2ecdec4ab33a46a60bd3
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-05-03 11:58:28 +0200

    Added dependencies to age3 and edited example.

--- src/main/java/org/age/mag/Sample.java
+++ src/main/java/org/age/mag/Sample.java
@@ -1,8 +1,16 @@
 package org.age.mag;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.age.services.identity.NodeDescriptor;
+
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.DistributedObject;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.ITopic;
 
 public class Sample {
 
@@ -11,6 +19,16 @@ public class Sample {
         clientConfig.addAddress("127.0.0.1:5701");
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         System.out.println(client.getName());
+        
+        Collection<DistributedObject> instances =  client.getDistributedObjects();
+        for(DistributedObject instance: instances){
+            System.out.println(instance.getServiceName());
+            if (instance instanceof IMap) {
+                System.out.println(((IMap) instance).values());
+            } else if (instance instanceof ITopic<?>) {
+                System.out.println(((ITopic) instance).getName());
+            }
+        }
     }
 
 }
